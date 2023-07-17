de41b1def07a8e467877c793b4c68a07801a05d3
Yash <yashshukla336@gmail.com>
2023-07-17 18:06:59 +0530

    first commit

--- Script.js
+++ Script.js
@@ -5,11 +5,11 @@ let numbers = [
 // Even -> Red
 // Odd -> Blue
 
-// function isodd(X) {
-//   return X % 2;
-// }
+function isodd(X) {
+  return X % 2;
+}
 
-// const store = numbers.filter(isodd).map((el) => {
+// const store = numbers.filter(isodd? ).map((el) => {
 //   console.log(el);
 //   return el;
 // });
@@ -21,23 +21,40 @@ let numbers = [
 // for (i = 0; i < store.length; i++) {
 //   neww.innerHTML = store;
 //   neww.style.backgroundColor = "blue";
+
 //   div.appendChild(neww);
+
 // }
 
-const cardContainer = document.querySelector(".Box");
+// const cardContainer = document.querySelector("#Box");
+
+const cardContainer = document.getElementById("Box");
 
 numbers.forEach((number) => {
-  const card = document.createElement("div");
-  card.classList.add("card");
+  const card = document.createElement("div"); // <div> </div>
+  card.classList.add("card"); //  <div class ="card"></div>
+  // 35-line will be like this or just above line   card.className= "card"
 
   if (number % 2 == 0) {
+    // 10 % 2, 5%2
     card.classList.add("red");
-    console.log(number);
+    card.style.height = "100px";
+    card.style.width = "100px";
   } else {
     card.classList.add("blue");
+    card.style.height = "100px";
+    card.style.width = "100px";
   }
-
   card.innerText = number;
 
+  card.style.display = "flex";
+  card.style.justifyContent = "center";
+  card.style.alignItems = "center";
+
   cardContainer.appendChild(card);
+
+  card.addEventListener("mouseover", function (e) {
+    const ss = e.target.innerText;
+    alert(ss);
+  });
 });
